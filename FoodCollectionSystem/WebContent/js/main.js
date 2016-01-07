96185b26f56277211f96264475ce1eb345fccd67
SHRIKANT <SHRIKANT@SHRIKANT-PC>
2016-01-07 08:55:33 +0530

    changes for sugnup

--- FoodCollectionSystem/WebContent/js/main.js
+++ FoodCollectionSystem/WebContent/js/main.js
@@ -9,8 +9,26 @@ $(document).ready(function(){
 			},
 			dataType : 'json',
 			success: function(result){
-				alert(result);
+				console.log(result);
+				/*localStorage.setItem("username",result.name);
+				localStorage.setItem("password",result.password);*/
+				$.ajax({
+					url:"admin/signup.html",
+					type: 'GET',
+					success: function(result){
+						console.log(result);
+						localStorage.setItem("username",result.name);
+						localStorage.setItem("password",result.password);
+						
+					}
+				});
+				
 			}
 		});
 	});
 });
+
+function checkLoginInfo(){
+	return localStorage.getItem('username')!=null && localStorage.getItem('password')!=null ;
+}
+
