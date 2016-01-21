3efd5da1c573e389ebfbae9020f6ce0d0c19be30
shghuge <shghuge@shghuge-ws01.partnet.cisco.com>
2016-01-21 20:01:34 +0530

    changes for table in user view

--- FoodCollectionSystem/WebRoot/admin/js/main.js
+++ FoodCollectionSystem/WebRoot/admin/js/main.js
@@ -56,9 +56,15 @@ $(document).ready(function(){
 				var htmlToRender = '<div class="sign-u "><div class="sign-up1" style="width: 7%; "><h4 maxLength="10">Sr No</h4></div><div class="sign-up1 " style="width: 30%; padding-left: 5%;"><h4>Name</h4></div><div class="sign-up1 " style="width: 20%; padding-left: 5%;"><h4>Contact No</h4></div><div class="sign-up1 " style="width: 25%; padding-left: 5%;"><h4>Email Id</h4></div><div class="sign-up1 " style="width: 16%; padding-left: 5%;"><h4>Status</h4><div class="clearfix"> </div></div>';
 				for(var i=0;i<response.user.length;i++){
 					htmlToRender= htmlToRender +'<div class="sign-u "><div class="sign-up1" style="width: 7%;"><h4>'+(i+1)+'</h4></div><div class="sign-up1 shriUserListBorder" style="width: 30%; padding-left: 5%;"><h4>'+response.user[i].name+'</h4></div><div class="sign-up1 shriUserListBorder" style="width: 20%; padding-left: 5%;"><h4>'+response.user[i].mobile+'</h4></div><div class="sign-up1 shriUserListBorder" style="width: 25%; padding-left: 5%;"><h4>'+response.user[i].email+'</h4></div><div class="sign-up1 shriUserListBorder" style="width: 16%; padding-left: 5%;"><h4>Yet to update</h4></div><div class="clearfix"> </div></div>';
+				}				
+				var htmlToRender1 = '<table id="t01" class="names"><tr><th>Sr No</th><th>Name</th><th>Contact No</th><th>Email Id</th><th>Status</th><th>Edit Button</th><th>Delete Button</th></tr>';
+				for(var i=0;i<response.user.length;i++){
+					htmlToRender1= htmlToRender1 +'<tr><td>'+(i+1)+'</td><td>'+response.user[i].name+'</td><td>'+response.user[i].mobile+'</td><td>'+response.user[i].email+'</td><td>Yet to update</td><td>Edit</td><td>Delete</td></tr>';
 				}
+				htmlToRender1= htmlToRender1 + '</table>';
 				
-				$("#mainTemplateBody").html(htmlToRender);
+				$("#mainTemplateBody").html(htmlToRender1);
+				$("#mainTemplateBody").css("width","100%");
 			}
 		});
 	});
