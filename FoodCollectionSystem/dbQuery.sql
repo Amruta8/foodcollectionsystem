519854456aedb49d5e2c7bfe5a0b76e4ebb803e9
shghuge <shghuge@shghuge-ws01.partnet.cisco.com>
2016-01-28 15:23:12 +0530

    db changes

--- FoodCollectionSystem/dbQuery.sql
+++ FoodCollectionSystem/dbQuery.sql
@@ -15,6 +15,24 @@ CREATE DATABASE IF NOT EXISTS `fcs_stage` /*!40100 DEFAULT CHARACTER SET latin1 
 USE `fcs_stage`;
 
 
+-- Dumping structure for table fcs_stage.collector_availability
+CREATE TABLE IF NOT EXISTS `collector_availability` (
+  `user_email` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  KEY `FK_collector_availability_user` (`user_email`),
+  CONSTRAINT `FK_collector_availability_user` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table fcs_stage.collector_availability: ~1 rows (approximately)
+DELETE FROM `collector_availability`;
+/*!40000 ALTER TABLE `collector_availability` DISABLE KEYS */;
+INSERT INTO `collector_availability` (`user_email`, `status`) VALUES
+	('qwe@gmail.com', 'Ideal'),
+	('ghuge.prashant213@gmail.com', 'Ideal'),
+	('123789', 'Ideal');
+/*!40000 ALTER TABLE `collector_availability` ENABLE KEYS */;
+
+
 -- Dumping structure for table fcs_stage.user
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(50) NOT NULL,
@@ -26,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table fcs_stage.user: ~10 rows (approximately)
+-- Dumping data for table fcs_stage.user: ~12 rows (approximately)
 DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`username`, `mobile`, `email`, `password`, `role`, `token`) VALUES
@@ -36,11 +54,14 @@ INSERT INTO `user` (`username`, `mobile`, `email`, `password`, `role`, `token`) 
 	('123', '123', '12345', '123', NULL, NULL),
 	('123', '123', '123456', '123', NULL, NULL),
 	('123', '123', '1234567', '123', NULL, NULL),
-	('admin', '', '2', 'shri', 'admin', '1102739406'),
+	('123', '123', '123789', '123', NULL, NULL),
+	('admin', '', '2', 'shri', 'admin', '1179621573'),
 	('shrikant', '', '3', NULL, NULL, NULL),
 	('asd', 'asd', 'asd', 'asd', NULL, NULL),
 	('asd', 'asd', 'asd1', 'asd', NULL, NULL),
-	('shrikant', '12346', 'ghuge@gmail.com', 'lkj', NULL, NULL);
+	('prashant ', '7897897898', 'ghuge.prashant213@gmail.com', '789789789', NULL, NULL),
+	('shrikant', '12346', 'ghuge@gmail.com', 'lkj', NULL, NULL),
+	('shrikant', '1231231233', 'qwe@gmail.com', 'qwe', NULL, NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
